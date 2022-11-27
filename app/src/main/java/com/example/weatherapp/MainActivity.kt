5e719de080766262fc647a7587343782d9c184e9
Jerridd-S <105670541+Jerridd-S@users.noreply.github.com>
2022-11-27 13:12:41 -0600

    Clean up

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -30,9 +30,3 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-//class MainActivity : AppCompatActivity() {
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_main)
-//    }
-//}
